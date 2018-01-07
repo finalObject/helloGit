50cd13a224e93c5ad9f011e6c30a839874053090
X. HU(Mac) <i@finalobject.cn>
2018-01-07 10:17:11 +0800

    hey

---
+++ notes.sh
@@ -0,0 +1,12 @@
+# '--' helps to declare that main.c is the name of a file
+# not a parameter or something else
+git checkout -- main.c
+
+# initialized empty Git repository in .git/
+git init
+
+# add file to repository
+git add index.html
+
+# as its name
+git status
