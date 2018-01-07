dd33d6bf77b5f483ed83141da1aba19264c79044
X. HU(Mac) <i@finalobject.cn>
2018-01-07 22:11:32 +0800

    hey

--- notes.sh
+++ notes.sh
@@ -10,6 +10,23 @@ git add index.html
 git commit -m "message"
 #or git commit --message="message"
 
+#this command will launch an editor to let you edit the detailed commit message
+git commit 
+export GIT_EDITOR
+
+#when index.html has been added before
+echo "something" >> index.html
+git commit index.html -m "commit without git add"
 
 # as its name
 git status
+
+git log
+git show [asfqbjwwgbwjqbwmbe124r]
+# get different between these two versions,default current and last verision
+git diff [asdsadwsad/addsadasda]
+
+git rm index.html
+
+git clone url
+git clone oldRepository newRepository
